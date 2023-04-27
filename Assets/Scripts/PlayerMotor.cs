289ad417b436486f240c531ef235deab181c2984
bastianfbr <badmax202@gmail.com>
2023-04-27 11:24:55 +0200

    feat: jump and run animation when jumping and walking

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class PlayerMotor : MonoBehaviour
 {
-	private CharacterController controller;
+    private CharacterController controller;
 	private Vector3 playerVelocity;
 	public float speed = 5f;
 	private bool isGrounded;
 	public float gravity = -9.81f;
 	public float jumpheight = 1f;
+    private Animator animator;
 
-	// Start is called before the first frame update
-	void Start()
+    // Start is called before the first frame update
+    void Start()
 	{
 		controller = GetComponent<CharacterController>();
-	}
+        animator = GetComponent<Animator>();
+    }
 
 	// Update is called once per frame
 	void Update()
@@ -29,19 +31,24 @@ public class PlayerMotor : MonoBehaviour
 		Vector3 movementDirection = Vector3.zero;
 		movementDirection.x = input.x;
 		movementDirection.z = input.y;
-		controller.Move(transform.TransformDirection(movementDirection) * speed * Time.deltaTime);
+
+        bool isMoving = movementDirection.magnitude > 0;
+        animator.SetBool("isWalking", isMoving);
+
+        controller.Move(transform.TransformDirection(movementDirection) * speed * Time.deltaTime);
 		playerVelocity.y += gravity * Time.deltaTime;
 		if(isGrounded && playerVelocity.y < 0)
 			playerVelocity.y = -2.0f;
-		controller.Move(playerVelocity * Time.deltaTime);
+        controller.Move(playerVelocity * Time.deltaTime);
 		Debug.Log(playerVelocity.y);
-	}
+    }
 
 	public void Jump()
 	{
-		if(isGrounded)
+        if (isGrounded)
 		{
 			playerVelocity.y = Mathf.Sqrt(jumpheight * -3.0f * gravity);
-		}
+            animator.SetBool("isJumping", true);
+        }
 	}
 }
